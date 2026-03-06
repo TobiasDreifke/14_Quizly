3cb91ba55d2fe80fbe637e93d0d40b1cabad6fd4
Tobias Dreifke <toobsecret@gmail.com>
2026-03-06 12:56:28 +0100

    feat: added urls

---
+++ auth_app/api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import RegistrationView, CookieTokenObtainPairView, CookieTokenRefreshView
+
+
+urlpatterns = [
+    path('registration/', RegistrationView.as_view(), name='registration'),
+    path('token/', CookieTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', CookieTokenRefreshView.as_view(), name='token_refresh'),
+
+]
