af4eb3b7c6db394c874d658b90680cb65d8f6ae8
Tobias Dreifke <toobsecret@gmail.com>
2026-03-06 14:09:30 +0100

    feat: added logout

--- auth_app/api/urls.py
+++ auth_app/api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import RegistrationView, CookieTokenObtainPairView, CookieTokenRefreshView
+from .views import RegistrationView, CookieTokenObtainPairView, CookieTokenRefreshView, LogoutView
 
 
 urlpatterns = [
     path('registration/', RegistrationView.as_view(), name='registration'),
     path('token/', CookieTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', CookieTokenRefreshView.as_view(), name='token_refresh'),
+    
+    path("logout/", LogoutView.as_view(), name="logout"),
 
 ]
