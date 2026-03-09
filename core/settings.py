472d990dfde348860c0702faca1a92f33a2964d0
Tobias Dreifke <toobsecret@gmail.com>
2026-03-09 13:05:15 +0100

    fix: enabled the .env in settings

--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from datetime import timedelta
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +26,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$-!odn=4c+$xc136eeycpmr!y8mx8de78k!jg9bku&*((ztuiw'
+# SECRET_KEY moved to .env
+SECRET_KEY = os.getenv('SECRET_KEY')
+# SECRET_KEY = "django-insecure-&&pl@+r+&!nz&m!ep6)i6$+3%wt^4)&h$l$1t6g2klnay9h4()"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+
+# API keys
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+WHISPER_KEY = os.getenv("WHISPER_KEY")
 
 
 # Application definition
