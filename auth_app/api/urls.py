8d1b3ddc044ab34e27c76d80dbe849b4a2eb8869
Tobias Dreifke <toobsecret@gmail.com>
2026-03-10 13:56:56 +0100

    fix: urls fixed

--- auth_app/api/urls.py
+++ auth_app/api/urls.py
@@ -3,8 +3,8 @@ from .views import RegistrationView, CookieTokenObtainPairView, CookieTokenRefre
 
 
 urlpatterns = [
-    path('registration/', RegistrationView.as_view(), name='registration'),
-    path('token/', CookieTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('register/', RegistrationView.as_view(), name='registration'),
+    path('login/', CookieTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', CookieTokenRefreshView.as_view(), name='token_refresh'),
     
     path("logout/", LogoutView.as_view(), name="logout"),
